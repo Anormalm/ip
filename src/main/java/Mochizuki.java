e38fa257a70d0e2b3d5342ce44fa80718a9e9ef1
Anormalm <e1480030@u.nus.edu>
2026-02-03 23:36:30 +0800

    "Level-1: Implement echo and personality tweaks to be Japanese"

--- src/main/java/Mochizuki.java
+++ src/main/java/Mochizuki.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Mochizuki {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(line);
-        System.out.println(" Hello! I'm Mochizuki");
-        System.out.println(" What can I do for you?");
+        System.out.println(" Konbanwa! I'm Mochizuki, keeper of the moonlit to-do list.");
+        System.out.println(" Tell me your command.");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(line);
+            if ("bye".equals(input)) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
     }
 }
