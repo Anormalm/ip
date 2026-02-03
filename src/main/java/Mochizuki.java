b2ed3c6826006321f6c8d7dcf9a23e154d477428
Anormalm <e1480030@u.nus.edu>
2026-02-03 23:43:49 +0800

    "Level-2: Implement task list"

--- src/main/java/Mochizuki.java
+++ src/main/java/Mochizuki.java
@@ -4,6 +4,8 @@ public class Mochizuki {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println(line);
         System.out.println(" Konbanwa! I'm Mochizuki, keeper of the moonlit to-do list.");
@@ -18,7 +20,25 @@ public class Mochizuki {
                 System.out.println(line);
                 break;
             }
-            System.out.println(" " + input);
+            if ("list".equals(input)) {
+                if (taskCount == 0) {
+                    System.out.println(" The shrine shelves are empty.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println(line);
+                continue;
+            }
+
+            if (taskCount < tasks.length) {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println(" added: " + input);
+            } else {
+                System.out.println(" The ledger is full. I can hold no more.");
+            }
             System.out.println(line);
         }
     }
