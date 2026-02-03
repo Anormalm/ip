c97328839e0061c5aaa43be69dcb70381f61c1da
Anormalm <e1480030@u.nus.edu>
2026-02-03 23:31:49 +0800

    "Level-0: Rename chatbot and implement greet-exit skeleton"

--- src/main/java/Mochizuki.java
+++ src/main/java/Mochizuki.java
@@ -1,8 +1,8 @@
-public class Atlas {
+public class Mochizuki {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
         System.out.println(line);
-        System.out.println(" Hello! I'm Atlas");
+        System.out.println(" Hello! I'm Mochizuki");
         System.out.println(" What can I do for you?");
         System.out.println(line);
         System.out.println(" Bye. Hope to see you again soon!");
